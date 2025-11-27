938207d58df306d127b446604b8e44b8ebe41d4b
LucasMedici <lucasrpmedici@gmail.com>
2025-11-27 10:52:49 -0300

    add: day 01 exercises

---
+++ src/day01/MaiorEntre3Numeros.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class MaiorEntre3Numeros {
+    public static void main(String[] args){
+    int num1 = 10;
+    int num2 = 55;
+    int num3 = 30;
+
+    int maior = num1;
+
+    if(num2 > maior){
+        maior = num2;
+    }
+
+    if(num3 > maior){
+        maior = num3;
+    }
+
+    System.out.println("O maior número é o: " + maior);
+    }
+}
---
+++ src/day01/ParOuImpar.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class ParOuImpar {
+    public static void main(String[] args){
+        int num = 20;
+        if(num % 2 == 0){
+            System.out.println("É par!");
+        } else {
+            System.out.println("É impar!");
+        }
+
+    }
+}
