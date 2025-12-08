ed996cd14a4e21861b7c9203ca1e5090ff70415c
LucasMedici <lucasrpmedici@gmail.com>
2025-12-08 15:43:03 -0300

    day 003 exercises

---
+++ src/day03/ArrayParaLista.java
@@ -0,0 +1,20 @@
+package day03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayParaLista {
+    public static void main(String[] args){
+        int[] array = new int[2];
+        array[0] = 2;
+        array[1] = 4;
+
+        List<Integer> lista = new ArrayList<>();
+
+        for(int i=0; i<array.length;i++){
+            lista.add(array[i]);
+        }
+
+        System.out.println(lista);
+    }
+}
---
+++ src/day03/DuplicarCadaElemento.java
@@ -0,0 +1,21 @@
+package day03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicarCadaElemento {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        System.out.println("ArrayList Inicial: "+ arrayList);
+
+        for(int i=0; i<arrayList.size(); i+=2 ){
+            arrayList.add(i+1, arrayList.get(i));
+        }
+
+        System.out.println("Array Final" + arrayList);
+
+    }
+}
