9af8fe2d2c578be6e4972dd8d431de9b709717d4
LucasMedici <lucasrpmedici@gmail.com>
2025-12-08 16:57:02 -0300

    day 04 ex1 hashmap keys duplicated solve

--- src/day04/CalculandoFrequenciaElemento.java
+++ src/day04/CalculandoFrequenciaElemento.java
@@ -6,13 +6,13 @@ public class CalculandoFrequenciaElemento {
     public static void main(String[] args) {
         HashMap<Integer, String> hashMap = new HashMap();
         hashMap.put(1, "Fallen");
-        hashMap.put(1, "Molodoy");
-        hashMap.put(1, "Yekindar");
-        hashMap.put(1, "Kscerato");
-        hashMap.put(1, "Yurih");
-        hashMap.put(1, "Fallen");
-        hashMap.put(1, "Fallen");
-        hashMap.put(1, "Molodoy");
+        hashMap.put(2, "Molodoy");
+        hashMap.put(3, "Yekindar");
+        hashMap.put(4, "Kscerato");
+        hashMap.put(5, "Yurih");
+        hashMap.put(6, "Fallen");
+        hashMap.put(7, "Fallen");
+        hashMap.put(8, "Molodoy");
 
         System.out.println(hashMap);
     }
