c1026d0ea8c92b6cb8abe7512c1ea4e946b87080
LucasMedici <lucasrpmedici@gmail.com>
2026-01-12 16:46:21 -0300

    day 06, ex01, ex2

---
+++ src/day06/SortingArrayComArraySort.java
@@ -0,0 +1,13 @@
+package day06;
+
+import java.util.Arrays;
+
+public class SortingArrayComArraySort {
+    public static void main(String[] args){
+        int[] array = {5, 10, 23, 11, 90, 1};
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
