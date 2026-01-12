cf969b945485c8afc3e7fa80e64877b61929f0d8
LucasMedici <lucasrpmedici@gmail.com>
2026-01-12 11:12:31 -0300

    day 05, ex01, ex02

---
+++ src/day05/CalcularVogaisEmString.java
@@ -0,0 +1,26 @@
+package day05;
+
+import java.util.HashMap;
+
+public class CalcularVogaisEmString {
+    public static void main(String[] args){
+        HashMap<String, Integer> mapVogais = new HashMap<>();
+        mapVogais.put("a", 0);
+        mapVogais.put("e", 0);
+        mapVogais.put("i", 0);
+        mapVogais.put("o", 0);
+        mapVogais.put("u", 0);
+
+        String palavra = "alemanha";
+
+        for(int i=0; i<palavra.length(); i++){
+            if(mapVogais.containsKey(String.valueOf(palavra.charAt(i)))){
+                String letra = String.valueOf(palavra.charAt(i));
+                mapVogais.put(letra, mapVogais.getOrDefault(letra, 0)+1);
+            }
+        }
+
+        System.out.println(mapVogais);
+
+    }
+}
